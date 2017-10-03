cd7c71ded999fb2fecdfaa527fa3f460bf1ebdcd
Moe Quraishi <mquraishi@causeforce.com>
2017-10-03 10:57:08 -0700

    fixes

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    const apiURL = 'https://secure2.convio.net/cfrca/site/SRGroupAPI?method=getGroupInfo&api_key=cfrca&login_name=apiadmin&login_password=welcome&v=1.0&response_format=json&group_id=225791';
+
+    request.get(apiURL, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            var locals = JSON.parse(body);
+            res.render('data', {test: locals.getGroupInfoResponse.groupInfo.numMembers});
+            console.log(locals.getGroupInfoResponse.groupInfo.numMembers);
+        }
+    });
 });
 
 //router.get('/alltables', function (req, res, next) {
