bfa4c6cac185b2ee87f310153c0166938b423e9a
Moe Quraishi <mquraishi@causeforce.com>
2017-11-02 14:21:50 -0700

    testing more

--- assets/app/tables/alltables.component.ts
+++ assets/app/tables/alltables.component.ts
@@ -29,17 +29,52 @@ export class AllTablesComponent {
 //        const getGroupInfo = 'method=getGroupInfo&api_key=cfrca&login_name=apiadmin&login_password=welcome&v=1.0&response_format=json&group_id=225791';
 //        this.getData(convioUrl + getGroupInfo, '.testing');
         $(window).on('load', function(){
+            // Toronto
             $('.to18-donations').append($('.hbs-to18-don').css('display','block'));
             $('.to18-regs').append($('.hbs-to18-reg').css('display','block'));
             $('.to18-crews').append($('.hbs-to18-crews').css('display','block'));
             $('.to18-riders').append($('.hbs-to18-riders').css('display','block'));
             $('.to18-rfi').append($('.hbs-to18-rfi').css('display','block'));
-            $('.to-riders').css('display', 'block');
-            console.log('before ajax call');
+            $('.to17-donations').append($('.hbs-to17-don').css('display','block'));
+            $('.to17-regs').append($('.hbs-to17-reg').css('display','block'));
+            $('.to17-crews').append($('.hbs-to17-crews').css('display','block'));
+            $('.to17-riders').append($('.hbs-to17-riders').css('display','block'));
+            $('.to17-rfi').append($('.hbs-to17-rfi').css('display','block'));
+            // Alberta
+            $('.ab18-donations').append($('.hbs-ab18-don').css('display','block'));
+            $('.ab18-regs').append($('.hbs-ab18-reg').css('display','block'));
+            $('.ab18-crews').append($('.hbs-ab18-crews').css('display','block'));
+            $('.ab18-riders').append($('.hbs-ab18-riders').css('display','block'));
+            $('.ab18-rfi').append($('.hbs-ab18-rfi').css('display','block'));
+            $('.ab17-donations').append($('.hbs-ab17-don').css('display','block'));
+            $('.ab17-regs').append($('.hbs-ab17-reg').css('display','block'));
+            $('.ab17-crews').append($('.hbs-ab17-crews').css('display','block'));
+            $('.ab17-riders').append($('.hbs-ab17-riders').css('display','block'));
+            $('.ab17-rfi').append($('.hbs-ab17-rfi').css('display','block'));
+            // Montreal
+            $('.mo18-donations').append($('.hbs-mo18-don').css('display','block'));
+            $('.mo18-regs').append($('.hbs-mo18-reg').css('display','block'));
+            $('.mo18-crews').append($('.hbs-mo18-crews').css('display','block'));
+            $('.mo18-riders').append($('.hbs-mo18-riders').css('display','block'));
+            $('.mo18-rfi').append($('.hbs-mo18-rfi').css('display','block'));
+            $('.mo17-donations').append($('.hbs-mo17-don').css('display','block'));
+            $('.mo17-regs').append($('.hbs-mo17-reg').css('display','block'));
+            $('.mo17-crews').append($('.hbs-mo17-crews').css('display','block'));
+            $('.mo17-riders').append($('.hbs-mo17-riders').css('display','block'));
+            $('.mo17-rfi').append($('.hbs-mo17-rfi').css('display','block'));
+            // Vancouver
+            $('.va18-donations').append($('.hbs-va18-don').css('display','block'));
+            $('.va18-regs').append($('.hbs-va18-reg').css('display','block'));
+            $('.va18-crews').append($('.hbs-va18-crews').css('display','block'));
+            $('.va18-riders').append($('.hbs-va18-riders').css('display','block'));
+            $('.va18-rfi').append($('.hbs-va18-rfi').css('display','block'));
+            $('.va17-donations').append($('.hbs-va17-don').css('display','block'));
+            $('.va17-regs').append($('.hbs-va17-reg').css('display','block'));
+            $('.va17-crews').append($('.hbs-va17-crews').css('display','block'));
+            $('.va17-riders').append($('.hbs-va17-riders').css('display','block'));
+            $('.va17-rfi').append($('.hbs-va17-rfi').css('display','block'));
+//            $('.to-riders').css('display', 'block');
             
-            $.get('../../views/data.hbs', function(data){
-                console.log(data);
-            });
         });
         if (window.location.href.indexOf('alltables') == -1) {
              location.reload(true);
--- routes/app.js
+++ routes/app.js
@@ -14,12 +14,47 @@ router.get('/alltables', function (req, res, next) {
         if (!err && response.statusCode == 200) {
             console.log(body);
             var locals = JSON.parse(body);
-            console.log(locals.getEventTotal.to18.totalDonation);
-            res.render('data', {to18Donations: locals.getEventTotal.to18.totalDonation,
-                               to18RegFee: locals.getEventTotal.to18.regFee,
-                               to18Crews: locals.getEventTotal.to18.crews,
-                               to18RFI: locals.getEventTotal.to18.rfi,
-                               to18Riders: locals.getEventTotal.to18.riders});
+            console.log(locals.getEventTotal.toronto.to17.crews);
+            res.render('data', {to18Donations: locals.getEventTotal.toronto.to18.totalDonation,
+                                to18RegFee: locals.getEventTotal.toronto.to18.regFee,
+                                to18Crews: locals.getEventTotal.toronto.to18.crews,
+                                to18RFI: locals.getEventTotal.toronto.to18.rfi,
+                                to18Riders: locals.getEventTotal.toronto.to18.riders,
+                                to17Donations: locals.getEventTotal.toronto.to17.totalDonation,
+                                to17RegFee: locals.getEventTotal.toronto.to17.regFee,
+                                to17Crews: locals.getEventTotal.toronto.to17.crews,
+                                to17RFI: locals.getEventTotal.toronto.to17.rfi,
+                                to17Riders: locals.getEventTotal.toronto.to17.riders, 
+                                mo18Donations: locals.getEventTotal.montreal.mo18.totalDonation,
+                                mo18RegFee: locals.getEventTotal.montreal.mo18.regFee,
+                                mo18Crews: locals.getEventTotal.montreal.mo18.crews,
+                                mo18RFI: locals.getEventTotal.montreal.mo18.rfi,
+                                mo18Riders: locals.getEventTotal.montreal.mo18.riders,
+                                mo17Donations: locals.getEventTotal.montreal.mo17.totalDonation,
+                                mo17RegFee: locals.getEventTotal.montreal.mo17.regFee,
+                                mo17Crews: locals.getEventTotal.montreal.mo17.crews,
+                                mo17RFI: locals.getEventTotal.montreal.mo17.rfi,
+                                mo17Riders: locals.getEventTotal.montreal.mo17.riders,
+                                ab18Donations: locals.getEventTotal.alberta.ab18.totalDonation,
+                                ab18RegFee: locals.getEventTotal.alberta.ab18.regFee,
+                                ab18Crews: locals.getEventTotal.alberta.ab18.crews,
+                                ab18RFI: locals.getEventTotal.alberta.ab18.rfi,
+                                ab18Riders: locals.getEventTotal.alberta.ab18.riders,
+                                ab17Donations: locals.getEventTotal.alberta.ab17.totalDonation,
+                                ab17RegFee: locals.getEventTotal.alberta.ab17.regFee,
+                                ab17Crews: locals.getEventTotal.alberta.ab17.crews,
+                                ab17RFI: locals.getEventTotal.alberta.ab17.rfi,
+                                ab17Riders: locals.getEventTotal.alberta.ab17.riders,
+                                va18Donations: locals.getEventTotal.vancouver.va18.totalDonation,
+                                va18RegFee: locals.getEventTotal.vancouver.va18.regFee,
+                                va18Crews: locals.getEventTotal.vancouver.va18.crews,
+                                va18RFI: locals.getEventTotal.vancouver.va18.rfi,
+                                va18Riders: locals.getEventTotal.vancouver.va18.riders,
+                                va17Donations: locals.getEventTotal.vancouver.va17.totalDonation,
+                                va17RegFee: locals.getEventTotal.vancouver.va17.regFee,
+                                va17Crews: locals.getEventTotal.vancouver.va17.crews,
+                                va17RFI: locals.getEventTotal.vancouver.va17.rfi,
+                                va17Riders: locals.getEventTotal.vancouver.va17.riders,});
 //            return locals.getGroupInfoResponse.groupInfo.numMembers;
         }
     });
