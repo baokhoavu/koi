e1b881625ff732d5dc0d6aaffcf3f393926d72ee
Moe Quraishi <mquraishi@causeforce.com>
2017-11-06 14:22:22 -0800

    Final Fixes for Launch

--- apidata.js
+++ apidata.js
@@ -3,7 +3,7 @@ const fixieRequest = request.defaults({'proxy': process.env.FIXIE_URL});
 var express = require('express');
 var router = express.Router();
 
-router.post('/apidata', function (req, res, next) {
+router.post('/', function (req, res, next) {
     
 db.products.save(
     { item: "envelopes", qty : 100, type: "Clasp" },
