16a4df9c62110edaab2c72c841c78003e9a2d621
Moe Quraishi <mquraishi@causeforce.com>
2017-11-07 08:17:39 -0800

    Final Fixes for Launch

--- apidata.js
+++ apidata.js
@@ -1,12 +1,12 @@
-const request = require('request');
-const fixieRequest = request.defaults({'proxy': process.env.FIXIE_URL});
-var express = require('express');
-var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://heroku_q1rgmlhw:6i8hl61vlc9g6ikqjcijmgscpv@ds157614.mlab.com:57614/heroku_q1rgmlhw/node-angular";
 
-router.post('/', function (req, res, next) {
-    
-db.products.save(
-    { item: "envelopes", qty : 100, type: "Clasp" },
-    { writeConcern: { w: "majority", wtimeout: 5000 } }
-)
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var myobj = { name: "Test", address: "Highway 37" };
+  db.collection("testcollection").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
 });
