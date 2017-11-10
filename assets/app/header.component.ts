adfd1a21e389af9ffb4cf645dcab0dc7fd06bc27
Moe Quraishi <mquraishi@causeforce.com>
2017-11-10 08:25:56 -0800

    Fixing Version Number

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -8,8 +8,8 @@ import { Router } from "@angular/router";
                     <header class="row">
                         <nav class="col-md-8 col-md-offset-2">
                             <ul class="nav nav-pills">
-                                <a *ngIf="!isLoggedIn()" class="navbar-brand" href="/"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: alpha</a>
-                                <a *ngIf="isLoggedIn()" class="navbar-brand" href="/alltables"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: alpha</a>
+                                <a *ngIf="!isLoggedIn()" class="navbar-brand" href="/"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: 1.0</a>
+                                <a *ngIf="isLoggedIn()" class="navbar-brand" href="/alltables"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: 1.0</a>
                                 <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['/auth']">Sign Up / Sign In</a></li>
                                 <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['/alltables']">Home</a></li>
                                 <li *ngIf="isLoggedIn()"><a (click)="onLogout()">Logout</a></li>
