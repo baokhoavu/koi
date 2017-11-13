a7aa6b883b602507e6c15df832237985fcc61747
Moe Quraishi <mquraishi@causeforce.com>
2017-11-13 13:13:42 -0800

    Remove Sign Up Button

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -4,19 +4,7 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-header',
-    template: `
-                    <header class="row">
-                        <nav class="col-md-8 col-md-offset-2">
-                            <ul class="nav nav-pills">
-                                <a *ngIf="!isLoggedIn()" class="navbar-brand" href="/"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: 1.0</a>
-                                <a *ngIf="isLoggedIn()" class="navbar-brand" href="/alltables"><img src="/img/koi_logo_v2.svg" alt="Koi Logo"> version: 1.0</a>
-                                <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['/auth']">Sign Up / Sign In</a></li>
-                                <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['/alltables']">Home</a></li>
-                                <li *ngIf="isLoggedIn()"><a (click)="onLogout()">Logout</a></li>
-                            </ul>
-                        </nav>
-                    </header>
-              `,
+    templateUrl: './header.component.html',
     styles: [`
             .navbar-brand img { width: 80px; 
                                 margin: 0 auto; }
