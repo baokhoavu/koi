41c6c40a01493ca3ed18665675004e2fe18eb512
Moe Quraishi <mquraishi@causeforce.com>
2017-11-20 08:21:33 -0800

    Fixed OneWalk Toronto Daily Walker Data

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,6 @@ const moment = require('moment');
 const router = express.Router();
 const data = require('../models/apidata');
 
-// mongoose.connect('mongodb://heroku_q1rgmlhw:6i8hl61vlc9g6ikqjcijmgscpv@ds157614.mlab.com:57614/heroku_q1rgmlhw/node-angular');
-
-// var db = mongoose.connection;
-
 router.get('/data', function(req, res) {
 	console.log('Requesting data...');
 
