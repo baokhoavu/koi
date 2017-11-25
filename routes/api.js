33cf13553b8d5c4e4b75914a77b3368bf6b6d79f
Moe Quraishi <mquraishi@causeforce.com>
2017-11-24 23:15:17 -0800

    data fixes

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,7 @@ router.get('/data', function(req, res) {
 	console.log('Requesting data...');
 	// Find data from yesterday with MomentJS (subtract 1 from Today's date)
 	data.find({"updated": moment().subtract(1, 'days').format('L')})
+		.sort({"_id": -1})
 		.exec(function(err, yesterday) {
 			if (err) {
 				console.log('Error getting data..');
