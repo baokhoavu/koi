7dd22e46686cfb15cd5a99d108cff484599d7928
Moe Quraishi <mquraishi@causeforce.com>
2018-03-23 07:40:53 -0700

    See Previous KOI Data Link only if logged in

--- assets/app/footer/footer.component.ts
+++ assets/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
     selector: 'app-footer',
@@ -7,7 +8,7 @@ import { Component } from "@angular/core";
 })
 export class FooterComponent {
 
-    constructor() {
+    constructor(private auth: AuthService) {
     }
 
 }
