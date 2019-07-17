8760269d023ca49d08f01c211e0b95f72f9b96cb
Bao Vu <baovu@Baos-MacBook-Pro.local>
2019-07-17 09:33:43 -0700

    redeploy

--- apidata.js
+++ apidata.js
@@ -409,7 +409,7 @@ promise.then(function(db) {
                                     ApiData.findOne(
                                         (err, data) => {
 
-                                            console.log("In the scheduler getting nightly data...");
+                                            console.log("In the scheduler getting nightly data..");
                                             
                                             // Set Variables for Real Time vs Static Event Data
 
