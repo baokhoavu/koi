bd8293bdec70a5e7ecc80a7b21cf9653f12e052a
baokhoavu <baokhoadinhvu@gmail.com>
2025-12-11 14:10:36 -0800

    improve data service with optional chaining and null checks

--- assets/app/data.service.ts
+++ assets/app/data.service.ts
@@ -1,50 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { Injectable } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 // import { HttpClient, Http, Response, Headers, RequestOptions } from "@angular/http";
-import { Observable } from "rxjs/Rx";
+import { Observable } from 'rxjs/Rx';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
-import {
-    HttpClient,
-    HttpRequest,
-    HttpEvent,
-    HttpEventType
-} from '@angular/common/http';
+import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
+import { environment } from './environment';
 
 @Injectable()
 export class DataService implements OnInit {
 	data: any = {};
-    allData: any = {};
+	allData: any = {};
 
 	interval: any;
 
-	allTable:boolean=true;
-	abTable:boolean=false;
-	toTable:boolean=false;
-	moTable:boolean=false;
-	vaTable:boolean=false;
-	prTable:boolean=false;
-	owtoTable:boolean=false;
-	mlTable:boolean=false;
-	brTable:boolean=false;
+	allTable: boolean = true;
+	abTable: boolean = false;
+	toTable: boolean = false;
+	moTable: boolean = false;
+	vaTable: boolean = false;
+	prTable: boolean = false;
+	owtoTable: boolean = false;
+	mlTable: boolean = false;
+	brTable: boolean = false;
 
-	myDate:string;
-	noData:boolean=false;
-	brisbaneData:boolean=false;
+	myDate: string;
+	noData: boolean = false;
+	brisbaneData: boolean = false;
 
-	public state:string = 'inactive';
+	public state: string = 'inactive';
 
 	private apiData = new BehaviorSubject<any>(null);
 	public apiData$ = this.apiData.asObservable();
 
-	constructor(private http: HttpClient) {
-	}
+	constructor(private http: HttpClient) {}
 
 	ngOnInit() {
 		// this.fetchData();
-		// this.interval = setInterval(() => { 
-  //           this.fetchData(); 
-  //       }, 1000);
+		// this.interval = setInterval(() => {
+		//           this.fetchData();
+		//       }, 1000);
 	}
 
 	// fetchData() {
@@ -56,51 +51,41 @@ export class DataService implements OnInit {
 	// }
 
 	fetchData() {
-		this.http.get('/api/data')
-			.subscribe(
-				(data) => {
+		this.http.get('/api/data').subscribe(
+			data => {
 				// console.log('Fetching data...');
-				// console.log(data.json());
-				this.data = data;
+				this.data = data ?? {};
 				// console.log(this.data);
-			}, (error) => {
-				console.log('There was an error getting the data.');
-				console.log(error);
-			});
+			},
+			error => {
+				console.log('There was an error getting the data.', error);
+			}
+		);
 	}
 
 	fetchAllData() {
-	    this.http.get('https://api.mlab.com/api/1/databases/heroku_q1rgmlhw/collections/apidatas?q={"updated": "'+ this.myDate +'"}&apiKey=DqLhKCxvsNwutqc_kQEYhHPsY5J4gWZ5')
-            .subscribe(
-                (data) => {
-                    this.allData = data;
-                    console.log(this.allData);
+		// Note: mLab is deprecated, consider migrating to MongoDB Atlas
+		this.http.get(environment.apiUrl + '/api/data-by-date?date=' + this.myDate).subscribe(
+			data => {
+				this.allData = data ?? [];
+				console.log(this.allData);
 
-                    if (this.allData.length === 0) {
-                        console.log('No data for that date range.');
-                        this.noData = true;
-                    }
-                    if (this.allData.length !== 0) {
-                        this.noData = false;
-                    }
-                    if (this.allData.length === 0 && this.allData.br18Riders) {
-                    	this.brisbaneData = false;
-					}
-                }, (error) => {
-                    if (error) {
-                        console.log('There was an error while getting all of the data.');
-                    }
-                }
-            );
-    }
+				const dataLength = this.allData?.length ?? 0;
+				this.noData = dataLength === 0;
+				this.brisbaneData = dataLength > 0 || !this.allData?.br18Riders;
+			},
+			error => {
+				console.log('There was an error while getting all of the data.', error);
+			}
+		);
+	}
 
-	setData(data) {
-		this.apiData.next(data);
-		console.log('Setting data...')
+	setData(data: any = null) {
+		this.apiData.next(data ?? null);
+		console.log('Setting data...');
 	}
 
 	toggleState() {
-        this.state = this.state === 'active' ? 'inactive' : 'active';
-    }
-
+		this.state = this.state === 'active' ? 'inactive' : 'active';
+	}
 }
