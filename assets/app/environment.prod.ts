119d40966255eef213c15b725e9eb257a97b550a
baokhoavu <baokhoadinhvu@gmail.com>
2025-12-11 14:07:15 -0800

    add environment config and editor settings

---
+++ assets/app/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+	production: true,
+	apiUrl: 'https://cfkoi.herokuapp.com',
+	mlabApiUrl: 'https://api.mlab.com/api/1/databases/heroku_q1rgmlhw/collections/apidatas',
+	mlabApiKey: 'DqLhKCxvsNwutqc_kQEYhHPsY5J4gWZ5'
+};
---
+++ assets/app/environment.ts
@@ -0,0 +1,10 @@
+// This file can be replaced during build by using environment-specific file.
+// The build system defaults to the dev environment which uses `environment.ts`.
+// For production builds, use `environment.prod.ts`.
+
+export const environment = {
+	production: false,
+	apiUrl: 'http://localhost:3000',
+	mlabApiUrl: 'https://api.mlab.com/api/1/databases/heroku_q1rgmlhw/collections/apidatas',
+	mlabApiKey: 'DqLhKCxvsNwutqc_kQEYhHPsY5J4gWZ5'
+};
